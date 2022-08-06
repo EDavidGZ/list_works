6f3f0a275b991b626117b2fdec7eb8d7fc71ce45
EDavidGZ <jgar05202415@gmail.com>
2022-08-05 19:34:40 -0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,52 @@
+import { useState } from 'react';
+import './App.css';
+import { Buscador } from './componentes/Buscador';
+import { Crear } from './componentes/Crear';
+import { Listado } from './componentes/Listado';
+
+function App() {
+    const [listadoState, setListadoState] = useState({})
+
+
+  return (
+    <div className="layout">
+    {/*Cabecera*/}
+    <header className="header">
+        <div className="logo">
+            <div className="play"></div>
+        </div>
+        
+        <h1>Lista de Tareas</h1>
+    </header>
+
+    {/*Barra de navegación*/}
+    <nav className="nav">
+        
+    </nav>
+
+    {/*Contenido principal*/}
+    <section id="content" className="content">
+
+        {/*aqui van las peliculas*/}
+        <Listado listadoState={listadoState} setListadoState={setListadoState}/>
+
+    </section>
+
+    {/*Barra lateral*/}
+    <aside className="lateral">
+       <Buscador listadoState={listadoState} setListadoState={setListadoState} />
+
+       <Crear setListadoState={setListadoState} />
+    </aside>
+
+    {/*Pie de página*/}
+    <footer className="footer">
+        &copy; Tareas.David - <a href="https://github.com/EDavidGZ?tab=repositories">Efren David Garza Zacamolpa</a>
+    </footer>
+
+</div>
+
+  );
+}
+
+export default App;
